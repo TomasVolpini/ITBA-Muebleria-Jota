89083638f2fa08496086008ff7648fc7013cf144
Tomás Volpini <tomasvolpini@gmail.com>
2025-10-02 23:32:32 -0300

    refactor: hago dinámico el puerto en server.js

--- server/server.js
+++ server/server.js
@@ -3,8 +3,9 @@ import express from "express";
 const app = express();
 
 app.use(express.json());
+const PORT = 5000;
 
 // CHICOS AQIUI: Agregar rutas y lógica de la API)
 
 app.get("/api/_health", (_req, res) => res.json({ ok: true }));
-app.listen(5000, () => console.log("API en http://localhost:5000"));
+app.listen(PORT, () => console.log(`API en http://localhost:${PORT}`));
