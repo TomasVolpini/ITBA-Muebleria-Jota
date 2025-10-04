d89766abe9a55318fd269989934661fd1cea58be
Tomás Volpini <tomasvolpini@gmail.com>
2025-10-03 22:43:08 -0300

    feat: hago fetch y obtengo los productos

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,25 @@
-import "./App.css";
+import "./styles/App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  // hago fetch al json que tiene la info de los productos y lo guardo en la variable "products".
+  useEffect(() => {
+    const url =
+      "https://raw.githubusercontent.com/TomasVolpini/ITBA-Muebleria-Jota/refs/heads/develop/server/database/productos.json";
+
+    try {
+      fetch(url)
+        .then((res) => res.json())
+        .then((data) => setProducts(data));
+    } catch (err) {
+      console.error("Error en el fetch de productos:", err.message);
+    }
+  }, []);
+  // nota: para insertar la imagen, usen esta url `https://raw.githubusercontent.com/TomasVolpini/ITBA-Muebleria-Jota/refs/heads/develop/server${product.imagen}`
+  // nota 2: seguramente haya que cambiar tanto la ruta del fetch como la de img cuando hagamos merge al main, porque ahora la base de datos a la que hacemos fetch está en la rama develop, la cual va a ser eliminada después de hacer merge
+
   return <></>;
 }
 
