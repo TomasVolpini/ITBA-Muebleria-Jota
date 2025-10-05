9c4dd06efd5769727804dce27ddb30901419ea31
Rafael Marcelo Ramírez Perazzolo <rramirezperazzolo@gmail.com>
2025-10-05 14:31:20 -0300

    feat: implementar componente Hero

---
+++ client/src/components/Hero.jsx
@@ -0,0 +1,19 @@
+const Hero = () => {
+    return (
+        <section className="hero--gradiente">
+            <div className="hero__content">
+                <h1 className="hero__title">
+                    Artesanía con alma y diseño único
+                </h1>
+                <p className="hero__subtitle">
+                    Descubrí productos creados para durar y enamorar.
+                </p>
+                <a href="/" className="btn-hero">
+                    Ver productos
+                </a>
+            </div>
+        </section>
+    );
+};
+
+export default Hero;
