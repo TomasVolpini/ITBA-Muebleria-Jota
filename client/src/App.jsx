16a736912c178f3eee06267da693865c15fb1dee
Rafael Marcelo Ramírez Perazzolo <rramirezperazzolo@gmail.com>
2025-10-05 13:54:01 -0300

    feat: Integración del Header en App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,16 @@
 import "./styles/App.css";
 import { useEffect, useState } from "react";
+import Header from "./components/Header";
 import Footer from "./components/Footer";
 import ProductList from "./components/ProductList";
 import ProductDetail from "./components/ProductDetail";
 
 function App() {
+  // Estados para productos y rutas 
   const [products, setProducts] = useState([]);
   const [ruta, setRuta] = useState("/");
-  const idBuscado = ruta;
-  const indice = products.findIndex((producto) => producto.id === idBuscado);
 
-  // hago fetch al json que tiene la info de los productos y lo guardo en la variable "products".
+  // Fetch de productos 
   useEffect(() => {
     const url =
       "https://raw.githubusercontent.com/TomasVolpini/ITBA-Muebleria-Jota/refs/heads/develop/server/database/productos.json";
@@ -23,22 +23,26 @@ function App() {
       console.error("Error en el fetch de productos:", err.message);
     }
   }, []);
-  // nota: para insertar la imagen, usen esta url `https://raw.githubusercontent.com/TomasVolpini/ITBA-Muebleria-Jota/refs/heads/develop/server${product.imagen}`
-  // nota 2: seguramente haya que cambiar tanto la ruta del fetch como la de img cuando hagamos merge al main, porque ahora la base de datos a la que hacemos fetch está en la rama develop, la cual va a ser eliminada después de hacer merge
 
-  console.log(ruta);
-  //nota 3: usar la variable "ruta" para el renderizado condicional de los componentes a detalle (ver clase del último jueves)
+  // Obtener el índice del producto según la ruta
+  const idBuscado = ruta;
+  const indice = products.findIndex((producto) => producto.id === idBuscado);
 
   return (
     <>
-      {ruta === "/" && <ProductList products={products} setRuta={setRuta} />}
-      {ruta !== "/" && (
+      <Header />
+      
+      {ruta === "/" && (
+        <ProductList products={products} setRuta={setRuta} />
+      )}
+
+      {ruta !== "/" && products[indice] && (
         <ProductDetail
-          products={products[indice]}
+          product={products[indice]}
           setRuta={setRuta}
-          ruta={ruta}
         />
       )}
+
       <Footer />
     </>
   );
