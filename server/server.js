c7dabd5510a978328c80ab9fd919c86d20c1fd3a
Tomás Volpini <tomasvolpini@gmail.com>
2025-11-03 22:32:33 -0300

    feat: conexión a la base de datos y al servidor

--- server/server.js
+++ server/server.js
@@ -1,18 +1,21 @@
 import express from "express";
 import productRoutes from "./routes/productRoutes.js";
+import cors from "cors";
 import { loger } from "./middleware/loger.js";
 import { notFound } from "./middleware/notFound.js";
 import { serverErrors } from "./middleware/serverErrors.js";
 import dotenv from "dotenv";
 dotenv.config({ path: "./server/.env" });
+import { connectToDB } from "./database/db.js";
 
 const app = express();
 app.use(express.json());
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
+// Falta agregar las limitaciones al cors, una vez subido a Render
 
-app.use(loger());
+app.use(loger);
 
 app.get("/", (req, res) => {
   res.send("¡Bienvenido al API de Mueblería Jota!");
@@ -20,7 +23,17 @@ app.get("/", (req, res) => {
 
 app.use("/api/products", productRoutes);
 
-app.use(notFound());
-app.use(serverErrors());
+app.use(notFound);
+app.use(serverErrors);
 
-app.listen(PORT, () => console.log(`API en http://localhost:${PORT}`));
+connectToDB()
+  .then(() => {
+    console.log("Base de datos conectada");
+    app.listen(PORT, () =>
+      console.log(`Servidor escuchando en el puerto ${PORT}`)
+    );
+  })
+  .catch((err) => {
+    console.error("Error al conectar a la base de datos:", err.message);
+    process.exit(1);
+  });
